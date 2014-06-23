676ae71053a29b967aaaa1d93e1f8d6ead318d17
James Morrin <jmorrin@sitepen.com>
2014-06-23 12:37:03 -0400

    Converted scrollTo tests to intern.
    This is not a one to one conversion. The original scrollTo tests were
    hard to convert to intern. These tests run through all of the document
    layout scenarios and verify that the target element is scrolled into the
    viewport.
    
    The target element is considered in view if the top or bottom and the
    right side of the target are within the viewport.

--- tests-intern/functional/window.js
+++ tests-intern/functional/window.js
@@ -2,18 +2,76 @@ define([
 	'require',
 	'dojo/Deferred',
 	'intern!object',
-	'intern/chai!assert'
+	'intern/chai!assert',
+	'tests-intern/functional/window/iframe_content/scrollDocuments',
 ], function (
 	require,
 	Deferred,
 	registerSuite,
-	assert
+	assert,
+	testScenarioDocuments
 ) {
 
-	registerSuite({
-		name: 'dojo/window',
+	/*
+	* There are many permutations of document layouts that need to be tested.
+	* This function will generate a test for every layout scenario
+	*/
+	function generateTestsForScenarios(suite) {
+		testScenarioDocuments.forEach(function (doc) {
+			suite[doc.label] = function () {
+				var visibleBefore;
+				var visibleAfter;
+				var hasScrolled;
 
-		'.getBox': {
+				return this.get('remote').elementById(doc.id)
+					.elementByClassName('before')
+					.getAttribute('value').then(function (visible) {
+						visibleBefore = parseInt(visible, 10);
+					})
+					.end()
+					.elementByClassName('scrollBtn')
+					.click()
+					.end()
+					.elementByClassName('hasScrolled')
+					.getAttribute('value').then(function (scrolled) {
+						hasScrolled = parseInt(scrolled, 10);
+					})
+					.end()
+					.elementByClassName('after')
+					.getAttribute('value').then(function (visible) {
+						visibleAfter = parseInt(visible, 10);
+					})
+					.then(function () {
+						if (hasScrolled) {
+							assert.notOk(visibleBefore, 'scrolled, target should not be visible before');
+							assert.ok(visibleAfter, 'scrolled, target should be visible after');
+						} else {
+							assert.ok(visibleBefore, 'not scrolled, target should be visible before');
+							assert.ok(visibleAfter, 'not scrolled, target should be visible after');
+						}
+					});
+			};
+		});
+	}
+
+	registerSuite(function () {
+
+		var suite = {
+			name: 'dojo/window'
+		};
+
+		suite['.scrollIntoView()'] = {
+			before: function () {
+				return this.get('remote')
+					.get(require.toUrl('./window/scroll.html'))
+					.setAsyncScriptTimeout(5000)
+					.waitForConditionInBrowser('ready');
+			}
+		};
+
+		generateTestsForScenarios(suite['.scrollIntoView()']);
+
+		suite['.getBox'] = {
 
 			before: function () {
 				return this.get('remote')
@@ -59,9 +117,10 @@ define([
 						assert.isTrue(viewportHeightAfter + 20  >= viewportHeightBefore, 'viewport didn\'t shrink, except for space taken by scrollbars');
 					});
 			}
+		};
 
-		}
-
+		return suite;
 	});
 
 });
+
